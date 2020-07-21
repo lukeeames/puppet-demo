dabdf3002ad3ca4f4e23d78d4affb3e292326682
lukeeames <66758614+lukeeames@users.noreply.github.com>
2020-07-22 09:32:33 +1000

    Updated site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,11 @@ node default {
 node 'ip-172-31-35-84.ap-southeast-2.compute.internal' {
   include role::master_server
 }
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
+}

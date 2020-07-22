1068557df5d110728662187775fa96ddc1581596
lukeeames <66758614+lukeeames@users.noreply.github.com>
2020-07-22 15:55:27 +1000

    Updated site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,9 @@ node default {
 }
 node 'ip-172-31-43-5.ap-southeast-2.compute.internal' {
   include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => $fqdn,
 }
 
 node /^web/ {
